0eb8a6b53ea556b17d7bf033d417cf6fed9509bb
Norman Jarvis <norman.jarvis@gmail.com>
2018-03-07 07:49:22 -0700

    Updated and added scripts to build each platform and copy it out to the www server

--- cr/buildForLinux.sh
+++ cr/buildForLinux.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 rm $GOPATH/bin/crlinux
 GOOS=linux GOARCH=386 go build -o $GOPATH/bin/crlinux main.go
-scp $GOPATH/bin/crlinux nsivraj@zytecoinc:/home/nsivraj/forge/bin/cr
+#scp $GOPATH/bin/crlinux nsivraj@www:/home/nsivraj/forge/bin/cr
---
+++ cr/copyTowww.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+scp $GOPATH/bin/crlinux nsivraj@www:/home/nsivraj/forge/bin/crlinux
+scp $GOPATH/bin/cr.exe nsivraj@www:/home/nsivraj/forge/bin/cr.exe
+scp $GOPATH/bin/cr nsivraj@www:/home/nsivraj/forge/bin/crmac
